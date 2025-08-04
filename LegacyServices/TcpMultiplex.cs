3d912f8e758d1723edb079881cd994301a8c4975
Kevin Gut <AyrA@users.noreply.github.com>
2025-08-04 20:06:13 +0200

    Add project files.

---
+++ LegacyServices/TcpMultiplex.cs
@@ -0,0 +1,11 @@
+﻿namespace LegacyServices;
+internal class TcpMultiplex
+{
+}
+
+internal abstract class BaseService
+{
+    public abstract void Start();
+    public abstract void Stop();
+    public abstract void Config();
+}
