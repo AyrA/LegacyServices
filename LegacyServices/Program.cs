bbd239f01d4cd2c86a852ecba0ba99adc13371cc
Kevin Gut <AyrA@users.noreply.github.com>
2025-08-05 07:51:54 +0200

    Nicer console layout

--- LegacyServices/Program.cs
+++ LegacyServices/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using LegacyServices;
-
-Console.WriteLine("Hello, World!");
+﻿using LegacyServices;
 
 List<BaseService> services = [
     new LegacyServices.TcpMultiplex.Service()
@@ -10,16 +7,52 @@ var configRoot = Path.Combine(AppContext.BaseDirectory, "Config");
 
 foreach (var s in services)
 {
-    Console.WriteLine("Configuring {0}...", s.Name);
-    s.Config(Path.Combine(configRoot, s.Name + ".json"));
+    Console.WriteLine("== {0} ==", s.Name);
+    Console.Write("Configuring ... ");
     try
     {
-        s.Start();
+        s.Config(Path.Combine(configRoot, s.Name + ".json"));
+        Ok();
     }
     catch (Exception ex)
     {
-        Console.WriteLine("Unable to start service. {0}", ex.Message);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Fail("Configuration failed. Reason: {0}", ex.Message);
+        continue;
+    }
+    if (s.IsReady)
+    {
+        Console.Write("Starting service ... ");
+        try
+        {
+            s.Start();
+            Ok();
+        }
+        catch (Exception ex)
+        {
+            Fail("Unable to start service. {0}", ex.Message);
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Service is disabled by configuration. Did not start");
     }
 }
 Console.WriteLine("Press CTRL+C to exit");
 Thread.CurrentThread.Join();
+
+static void Ok()
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("[OK]");
+    Console.ResetColor();
+}
+
+static void Fail(string format, params object?[] args)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("[FAIL]");
+    Console.WriteLine(format, args);
+    Console.ResetColor();
+}
